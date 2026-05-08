fe29087f5e89fe558dc2ef4782b7cd43ee67d008
Dale Robbennolt <dar4836@utexas.edu>
2026-05-08 15:12:33 -0500

    adds all components for mdcev including backend procedures for logit distribution functions and model structure with examples for linear and tranditional MDCEV models

---
+++ src/pybhatlib/models/mdcev/__init__.py
@@ -0,0 +1,19 @@
+"""Multiple Discrete-Continuous Extreme Value (MDCEV) model.
+
+Supports both the traditional outside-good utility specification
+(Bhat, 2008) and the linear outside-good utility specification
+(Bhat, 2018) via ``MDCEVControl.utility``.
+"""
+
+from pybhatlib.models.mdcev._mdcev_control import MDCEVControl
+from pybhatlib.models.mdcev._mdcev_results import MDCEVResults
+from pybhatlib.models.mdcev._mdcev_model import MDCEVModel
+from pybhatlib.models.mdcev._mdcev_ate import MDCEVATEResult, mdcev_ate
+
+__all__ = [
+    "MDCEVControl",
+    "MDCEVResults",
+    "MDCEVModel",
+    "MDCEVATEResult",
+    "mdcev_ate",
+]
